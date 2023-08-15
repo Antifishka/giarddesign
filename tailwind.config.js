8772eeb615283e5abefdb9f7ed2f5b12be9d0a55
Antifishka <101981622+Antifishka@users.noreply.github.com>
2023-08-16 00:22:17 +0300

    add hero, sprite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,23 @@ export default {
   content: ["./**/*.{html,js}"],
   theme: {
     screens: {
-      'sm': '480px',
-      'lg': '1440px',
+      'sm': {'max': '480px'},
+      'xl': '1440px',
     },
     colors: {
       primary: "#111111",
+      secondary: "#F5F0EC",
       accent: "#1B5B31",
+      background: "#DCC1AB",
     },
     width: {
       lg: "1440px",
     },
     padding: {
       lg: "89px",
+    },
+    fontFamily: {
+      'title': ['"Montserrat",sans-serif'],
     },
     extend: {
     },
