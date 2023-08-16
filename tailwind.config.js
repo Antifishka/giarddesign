044bab005db10417870f15012b6b549722fa1660
Antifishka <101981622+Antifishka@users.noreply.github.com>
2023-08-16 16:25:33 +0300

    add hero buttons, offer section markup

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,16 @@ export default {
       secondary: "#F5F0EC",
       accent: "#1B5B31",
       background: "#DCC1AB",
+      white: "#FFFFFF",
+      black: "#000000",
+      transparent: "transparent",
     },
     width: {
       lg: "1440px",
     },
     padding: {
       lg: "89px",
+      hero: " calc(100vw / 2 - 64px - 599px)"
     },
     fontFamily: {
       'title': ['"Montserrat",sans-serif'],
