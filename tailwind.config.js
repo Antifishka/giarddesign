6e086a5f79bd9afe5a6f3255652daf5e488fe11d
Antifishka <101981622+Antifishka@users.noreply.github.com>
2023-08-18 12:18:32 +0300

    add config, search button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js}", "!./**/node_modules/**"],
   theme: {
     screens: {
       'sm': {'max': '480px'},
