06d8490175e96de73bd5b3eca72ae1635a3eeb47
Antifishka <101981622+Antifishka@users.noreply.github.com>
2023-08-22 14:38:57 +0300

    add function for load more button

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,36 @@
-const slider = document.querySelector('.hero__slider');
-const prev = document.querySelector('.hero__btn--prev');
-const next = document.querySelector('.hero__btn--next');
+import { refs } from './refs.js';
+import { appendImagesMarkup } from "./appendImagesMarkup.js";
+
+// Add images
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+
+function onLoadMoreBtn() {
+    appendImagesMarkup();
+
+    macyInstance.runOnImageLoad(() =>
+        macyInstance.recalculate(true), true);
+
+    refs.loadMoreBtn.disabled = true;
+}
 
 // Siema
 const mySiema = new Siema({
-    selector: slider,
+    selector: refs.slider,
     duration: 1000,
     easing: 'cubic-bezier(.17,.67,.32,1.34)',
     loop: true,
     onInit: () => { console.log("Siema work!")},
 });
 
-prev.addEventListener('click', () => mySiema.prev())
-next.addEventListener('click', () => mySiema.next());
+refs.prev.addEventListener('click', () => mySiema.prev())
+refs.next.addEventListener('click', () => mySiema.next());
 
 // AOS
 AOS.init();
 
 // Macy
 const macyInstance = Macy({
-    container: '.projects__list',
+    container: refs.gallery,
     columns: 3,
     margin: 44,
     trueOrder: true,
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export const refs = {
+    slider: document.querySelector('.hero__slider'),
+    prev: document.querySelector('.hero__btn--prev'),
+    next: document.querySelector('.hero__btn--next'),
+    gallery: document.querySelector('.projects__list'),
+    loadMoreBtn: document.querySelector('.projects__btn'),
+};
